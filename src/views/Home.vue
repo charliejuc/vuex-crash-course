b0e2026d39e716f796b2f4799fab50929ae30032
charliejuc <charliejuc@gmail.com>
2019-12-13 15:49:36 +0100

    modified:   src/views/Home.vue Adds shortDescription computed property.
    Adds ellipsize method, adds etc points to the end when string.length is
    less than descriptionMaxChars variable.
    
    Adds description to data and textarea with v-model for description.
    
    Adds css base styles.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template lang="pug">
   .home 
     h1 {{ title.toUpperCase() }} | Company Inc.
-    input(
-      type="text"
-      @keyup="updateTitle(newTitle)"
-      v-model="newTitle"
-    )
+    //- p {{ description.slice(0, 30) + '...' }}
+    p {{ shortDescription }}
+
+    .inputs
+      .input
+        input(
+          type="text"
+          @keyup="updateTitle(newTitle)"
+          v-model="newTitle"
+        )
+      .input
+        textarea(
+          v-model="description"
+        )
     //- button(
     //-   @click="updateTitle(newTitle)"
     //- ) Write Title
@@ -14,6 +23,7 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+const descriptionMaxChars = 60
 
 export default {
   name: 'Home',
@@ -21,15 +31,28 @@ export default {
     const title = 'Vue Crash Course'
     return {
       title,
-      newTitle: title
+      newTitle: title,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }
   },
   created() {
     // setTimeout(() => this.updateTitle('Título Ninja 2'), 3000)
   },
+  computed: {
+    shortDescription() {
+      return this.ellipsize(this.description)
+    }
+  },
   methods: {
     updateTitle(title) {
       this.title = title
+    },
+    ellipsize(text) {
+      if ( typeof(text) !== 'string' || text.length <= descriptionMaxChars ) {
+        return text
+      }
+
+      return `${text.slice(0, descriptionMaxChars)}...`
     }
   }
   // components: {
@@ -37,3 +60,40 @@ export default {
   // }
 }
 </script>
+
+<style lang="scss">
+  .home {
+    width: 850px;
+    margin: 0 auto;
+  }
+
+  .inputs {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .input {
+    box-sizing: border-box;
+    margin-top: 20px;
+    padding: 0 10px;
+    width: inherit;
+
+    textarea, input {
+      box-sizing: border-box;
+      width: 100%;
+      padding: 10px;
+      border-radius: .5rem;
+    }
+
+    textarea {
+      max-height: 500px;
+      min-height: 200px;
+
+      max-width: 100%;
+      min-width: 100%;
+    }
+  }
+
+</style>
